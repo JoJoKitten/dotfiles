ff79cc89fe5a22ffc18782b28783952e7cb4ffa1
Johannes Bensmann <johannesbensmann@gmx.de>
2018-12-26 17:21:55 +0100

    removed some package

--- install_packages.sh
+++ install_packages.sh
@@ -26,49 +26,65 @@ xorg-server
 xorg-xinit
 zsh"
 
-yaourt_packages="
+aur_packages="
 polybar
-megasync"
+dropbox"
 
 # install git (which is used to install trizen)
 echo "Installing git ..."
-pacman --noconfirm -S git &> /dev/null
-
-# install trizen (AUR helper) first because it needs the user
-# to enter the password once
-echo "Installing trizen ..."
-rm -rf /tmp/trizen
-sudo -u jbensmann git clone https://aur.archlinux.org/trizen.git /tmp/trizen &> /dev/null
-cd /tmp/trizen
-sudo -u jbensmann makepkg -si --noconfirm &> /dev/null
+sudo pacman --noconfirm -S git &> /dev/null
 
 # install packages from the main repos
 for p in $packages; do
     echo "Installing $p ..."
-    pacman --noconfirm -S $p &> /dev/null
+    sudo pacman --noconfirm -S $p &> /dev/null
 done
 
+# install trizen (AUR helper)
+if ! hash trizen; then
+    echo "Installing trizen ..."
+    cd /tmp
+    git clone https://aur.archlinux.org/trizen.git &> /dev/null
+    cd trizen
+    sudo makepkg -si &> /dev/null
+fi
+
 # install packages from AUR
-for p in $yaourt_packages; do
+for p in $aur_packages; do
     echo "Installing $p from AUR ..."
     trizen --noconfirm -S $p &> /dev/null
 done
 
 # install oh-my-zsh
-echo "Installing oh-my-zsh ..."
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+if [ ! -d ~/.oh-my-zsh ]; then
+    echo "Installing oh-my-zsh ..."
+    sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+fi
 
 # install spacemacs
-echo "Installing spacemacs ..."
-rm -r ~/.emacs.d
-git clone https://github.com/syl20bnr/spacemacs ~/.emacs.d
+if [ ! -f ~/.emacs.d/spacemacs.mk ]; then
+    echo "Installing spacemacs ..."
+    # rm -rf ~/.emacs.d
+    # git clone https://github.com/syl20bnr/spacemacs ~/.emacs.d
+fi
 
 # dotfiles
-echo "Installing dotfiles"
-git clone --recurse-submodules https://github.com/jojokitten/dotfiles.git ~/dotfiles
-cd ~/dotfiles
-~/dotfiles/install_requirements.sh
-~/dotfiles/dotdrop.sh install -p laptop-i3
+cd
+if [ ! -d dotfiles ]; then
+    echo "Installing dotfiles"
+    git clone --recurse-submodules https://github.com/jojokitten/dotfiles.git dotfiles
+    cd dotfiles
+    ./install_requirements.sh
+    ./dotdrop.sh install -p laptop-i3
+fi
+
+# entr
+if ! hash entr; then
+    cd /tmp
+    wget http://eradman.com/entrproject/code/entr-4.1.tar.gz
+    tar -xf entr-4.1.tar.gz
+    cd eradman-entr-* && ./configure && make test && sudo make install
+fi
 
 echo "Done!"
 
