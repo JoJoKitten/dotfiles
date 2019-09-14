7beb01a287acdb7ed428daca3f26e35f948ef536
Johannes Bensmann <johannesbensmann@gmx.de>
2019-09-14 15:54:18 +0200

    removed click wibox

--- dotfiles/config/awesome/mywidgets/ram.lua
+++ dotfiles/config/awesome/mywidgets/ram.lua
@@ -12,29 +12,6 @@ local ram_widget = wibox.widget {
     widget = wibox.widget.piechart
 }
 
---- Widget which is shown when user clicks on the ram widget
-local w = wibox {
-    height = 200,
-    width = 400,
-    ontop = true,
-    expand = true,
-    bg = '#1e252c',
-    max_widget_size = 500
-}
-
-w:setup {
-    border_width = 0,
-    colors = {
-        '#5ea19d',
-        '#55918e',
-        '#4b817e',
-    },
-    display_labels = false,
-    forced_width = 25,
-    id = 'pie',
-    widget = wibox.widget.piechart
-}
-
 local total, used, free, shared, buff_cache, available, total_swap, used_swap, free_swap
 
 local function getPercentage(value)
@@ -60,21 +37,6 @@ watch('bash -c "LANGUAGE=en_US.UTF-8 free | grep -z Mem.*Swap.*"', 5,
         end
     end,
     ram_widget
-)
-
-ram_widget:buttons(
-    awful.util.table.join(
-        awful.button({}, 1, function()
-            awful.placement.top_right(w, { margins = {top = 25, right = 10}, parent = awful.screen.focused() })
-            w.pie.data_list = {
-                {'used ' .. getPercentage(used + used_swap), used + used_swap},
-                {'free ' .. getPercentage(free + free_swap), free + free_swap},
-                {'buff_cache ' .. getPercentage(buff_cache), buff_cache}
-            }
-            w.pie.display_labels = true
-            w.visible = not w.visible
-        end)
-    )
 )
 
 return ram_widget
