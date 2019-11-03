b56e3a7430dec37dec23f72d31a0e56a5fdd1b37
Johannes Bensmann <johannesbensmann@gmx.de>
2019-11-03 20:16:33 +0100

    switched to vim-plug

--- install_packages.sh
+++ install_packages.sh
@@ -96,9 +96,10 @@ cd dotfiles
 ./install_requirements.sh
 ./dotdrop.sh install -f -p laptop-i3
 
-# Vundle (vim plugin manager)
-if [ ! -d ~/.vim/bundle/Vundle.vim ]; then
-    git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+# vim-plug
+if [ ! -f ~/.local/share/nvim/site/autoload/plug.vim ]; then
+    curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
+        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 fi
 
 # entr
