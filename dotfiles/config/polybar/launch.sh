359c5c2b5ceb69653dab85679547b5c2b6c62441
Johannes Bensmann <johannesbensmann@gmx.de>
2020-01-13 09:41:38 +0100

    minor change

--- dotfiles/config/polybar/launch.sh
+++ dotfiles/config/polybar/launch.sh
@@ -7,9 +7,7 @@ killall -q polybar
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
 # Launch bars
-for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
-    MONITOR=$m polybar --reload top &
-done
+polybar top &
 
 {%@@ if monitor2 is defined @@%}
 polybar top2
